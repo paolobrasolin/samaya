a405996134a7071795c8d0a56dd413da850ac719
Paolo Brasolin <paolo.brasolin@gmail.com>
2016-06-27 18:26:26 +0200

    lots of grunt goodness added

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,117 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    buildFolder: 'build',
+    srcFolder: 'src',
+
     uglify: {
-      build: {
-        src: 'search.js',
-        dest: 'search.min.js'
-      }
-    }
+      files: { 
+        src: '<%= srcFolder %>/*.js',
+        dest: '<%= buildFolder %>/',
+        expand: true,
+        flatten: true,
+        ext: '.js',
+      },
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: [{
+           cwd: '<%= srcFolder %>',
+           src: '**/*.html',
+           dest: '<%= buildFolder %>/',
+           expand: true,
+        }],
+      },
+    },
+
+    jsbeautifier : {
+      files : "<%= srcFolder %>/*",
+      options : {
+        js: {
+          indentSize: 2,
+          preserveNewlines: false,
+        },
+        css: {
+          indentSize: 2,
+        },
+        html: {
+          indentSize: 2,
+          preserveNewlines: false,
+        },
+      },
+    },
+
+    jshint: {
+      myFiles: ['<%= srcFolder %>/*.js'],
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+      },
+      all: {
+        files: '<%= srcFolder %>/*',
+//        tasks: [ 'copy' ],
+      },
+//      scripts: {
+//        files: ['js/*.js'],
+//        tasks: ['uglify'],
+//        options: {
+//          spawn: false,
+//        },
+//      } 
+    },
+
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= srcFolder %>/',
+            src: ['**'],
+            dest: '<%= buildFolder %>/',
+          },
+        ],
+      },
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 1337,
+          base: 'src',
+          livereload: true,
+          open: true,
+        },
+      },
+    },
+
+    'gh-pages': {
+      options: {
+        base: '<%= buildFolder %>',
+      },
+      src: ['**'],
+    },
+
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('serve', ['connect', 'watch']);
+  grunt.registerTask('deploy', ['copy', 'gh-pages']);
 
 };
 
---
+++ src/samay.js
@@ -0,0 +1,82 @@
+var Samay = (function() {
+  var latitude;
+  var longitude;
+  var now;
+  var sunrise;
+  var sunset;
+  var daytime;
+  var prahar;
+  var fetchCoordinates = function() {
+    return new Promise(function(resolve, reject) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      } else {
+        reject(Error("Geolocation is not supported by this browser"));
+      }
+    });
+  };
+  var update = function() {
+    return new Promise(function(resolve, reject) {
+      fetchCoordinates().then(function(geoposition) {
+        latitude = geoposition.coords.latitude;
+        longitude = geoposition.coords.longitude;
+        return fetchTwilight('today');
+      }).then(function(response) {
+        sunrise = new Date(response.results.sunrise);
+        sunset = new Date(response.results.sunset);
+        now = new Date();
+        if (now <= sunrise) {
+          return fetchTwilight('yesterday').then(function(response) {
+            sunset = new Date(response.results.sunset);
+            daytime = false;
+          });
+        } else if (sunset <= now) {
+          return fetchTwilight('tomorrow').then(function(response) {
+            sunrise = new Date(response.results.sunrise);
+            daytime = false;
+          });
+        } else {
+          daytime = true;
+        }
+      }).then(function() {
+        return calculatePrahar();
+      }).then(function() {});
+    });
+  };
+  var fetchTwilight = function(date) {
+    return new Promise(function(resolve, reject) {
+      $.ajax({
+        url: 'http://api.sunrise-sunset.org/json',
+        dataType: "json",
+        data: {
+          lat: latitude,
+          lng: longitude,
+          date: date,
+          formatted: "0",
+        },
+      }).done(function(json) {return resolve(json);})
+        .fail(function(xhr, status, err) {return reject(status + err.message);});
+    });
+  };
+  var calculatePrahar = function() {
+    return new Promise(function(resolve, reject) {
+      if (daytime) {
+        prahar = 1 + Math.floor(4 * (now - sunrise) / (sunset - sunrise));
+      } else {
+        prahar = 5 + Math.floor(4 * (now - sunset) / (sunrise - sunset));
+      }
+    });
+  };
+  return {
+    update: update,
+    prahar: function() {
+      return prahar;
+    },
+    //    now: function(){ return now; },
+    //    lat: function(){ return latitude; },
+    //    lng: function(){ return longitude; },
+    //    sunrise: function(){ return sunrise; },
+    //    sunset: function(){ return sunset; },
+    //    daytime: function(){ return daytime; },
+  };
+})();
