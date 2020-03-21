0b4e146d4dbf0dca0d4a2f56e53c8b3ae613fc01
Paolo Brasolin <paolo.brasolin@gmail.com>
2020-03-21 17:40:41 +0100

    chore: update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,121 +1,79 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
-    srcFolder: 'src',
-    distFolder: 'dist',
+    srcFolder: "src",
+    distFolder: "dist",
 
     htmlmin: {
       dist: {
         options: {
           removeComments: true,
-          collapseWhitespace: true,
+          collapseWhitespace: true
         },
-        files: [{
-           cwd: '<%= distFolder %>',
-           src: '*.html',
-           dest: '<%= distFolder %>',
-           expand: true,
-        }],
-      },
-    },
-
-    jsbeautifier : {
-      files : "<%= srcFolder %>/*",
-      options : {
-        js: {
-          indentSize: 2,
-          preserveNewlines: false,
-        },
-        css: {
-          indentSize: 2,
-        },
-        html: {
-          indentSize: 2,
-          preserveNewlines: false,
-        },
-      },
-    },
-
-    jshint: {
-      myFiles: ['<%= srcFolder %>/*.js'],
+        files: [
+          {
+            cwd: "<%= distFolder %>",
+            src: "*.html",
+            dest: "<%= distFolder %>",
+            expand: true
+          }
+        ]
+      }
     },
 
     watch: {
       options: {
-        livereload: true,
+        livereload: true
       },
       all: {
-        files: '<%= srcFolder %>/*',
-        tasks: [ 'build' ],
-      },
+        files: "<%= srcFolder %>/*",
+        tasks: ["build"]
+      }
     },
-
-//    copy: {
-//      main: {
-//        files: [
-//          {
-//            expand: true,
-//            cwd: '<%= srcFolder %>',
-//            src: '*.json',
-//            dest: '<%= distFolder %>',
-//          },
-//        ],
-//      },
-//    },
 
     connect: {
       server: {
         options: {
           port: 1337,
-          base: 'dist',
+          base: "dist",
           livereload: true,
-          open: true,
-        },
-      },
+          open: true
+        }
+      }
     },
 
-    'gh-pages': {
+    "gh-pages": {
       options: {
-        base: '<%= distFolder %>',
-        message: 'Grunt-generated commit',
+        base: "<%= distFolder %>",
+        message: "Grunt-generated commit"
       },
-      src: ['**'],
+      src: ["**"]
     },
 
     inline: {
       dist: {
-        options:{
+        options: {
           uglify: true,
-          cssmin: true,
+          cssmin: true
         },
-        src: 'src/index.html',
-        dest: 'dist/index.html',
+        src: "src/index.html",
+        dest: "dist/index.html"
       }
-    },
-
-
+    }
   });
 
-//  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-inline');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-inline");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-connect");
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.loadNpmTasks("grunt-gh-pages");
 
-  grunt.loadNpmTasks('grunt-gh-pages');
-
-
-  grunt.registerTask('build', ['inline', 'htmlmin']);
-  grunt.registerTask('serve', ['connect', 'watch']);
-  grunt.registerTask('deploy', ['build', 'gh-pages']);
-
+  grunt.registerTask("build", ["inline", "htmlmin"]);
+  grunt.registerTask("serve", ["connect", "watch"]);
+  grunt.registerTask("deploy", ["build", "gh-pages"]);
 };
-
